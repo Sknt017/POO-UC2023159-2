ac096a363ddadcbe263ea1b120148d6687c5c697
sknt017 <dsanti1882@gmail.com>
2024-01-16 21:54:56 -0500

    pro update

--- src/main/java/Procedimientos/Usuario.java
+++ src/main/java/Procedimientos/Usuario.java
@@ -22,6 +22,7 @@ public class Usuario {
         //throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
+    
     public Usuario() {
         //throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
