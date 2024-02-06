dfe1f0dc51c3517968e078a4b9e29e91a38feeee
SKNT017 <dsanti1882@gmail.com>
2024-02-06 11:11:27 -0500

    adding unique id generator functionality

--- src/main/java/Procedimientos/Referencias.java
+++ src/main/java/Procedimientos/Referencias.java
@@ -4,12 +4,30 @@
  */
 package Procedimientos;
 
+import java.awt.List;
+import java.util.ArrayList;
+import java.util.Random;
+
 /**
  *
  * @author dasan
  */
 public class Referencias {
     
+    public static void main(String[]args){
+        int number;
+        ArrayList<String> numRF = new ArrayList<>();
+        Random random = new Random();
+        for(int i=0;i<10;i++){
+            number = random.nextInt(10);
+//            System.out.println(number);
+            numRF.add(String.valueOf(number));
+        }
+        //System.out.println(numRF);
+        //String RID = TextUtils.join(", ", List);
+        String listString = String.join("-",numRF);
+        System.out.println(listString);
+    }
     public void obtenerReferencia(int Refe){
         
     }
