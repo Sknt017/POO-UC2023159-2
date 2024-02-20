67974e5998d9a11c6b7b7966e163715c0b1d4cf9
sknt017 <dsanti1882@gmail.com>
2024-02-20 09:11:19 -0500

    added procedure to the reservation system

--- src/main/java/Procedimientos/Reserva.java
+++ src/main/java/Procedimientos/Reserva.java
@@ -3,13 +3,21 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Procedimientos;
-
-import Interaccion.VenDetallesFin;
-
 /**
  *
  * @author dasan
  */
 public class Reserva{
+    public Reserva(String desdeHoraCB, String hastaHoraCB, int Sdia, int Smes, int Sanio, String Copt) {
+        String HoraReservacion = desdeHoraCB;
+        String HoraReservacionFin = hastaHoraCB;
+        int diaReserva = Sdia;
+        int mesReserva = Smes;
+        int anioReserva = Sanio;
+        String canchaReserva = Copt;
+        String[] ResF = {HoraReservacion,HoraReservacionFin,String.valueOf(diaReserva),String.valueOf(mesReserva),String.valueOf(anioReserva),canchaReserva};
+        
+        //throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
     
 }
