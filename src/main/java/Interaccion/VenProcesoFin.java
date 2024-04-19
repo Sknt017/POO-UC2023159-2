cac6fd84233821f5a1a9a759551d99c255439d16
sknt017 <dsanti1882@gmail.com>
2024-04-18 23:49:20 -0500

    rem comen code

--- src/main/java/Interaccion/VenProcesoFin.java
+++ src/main/java/Interaccion/VenProcesoFin.java
@@ -116,15 +116,6 @@ public class VenProcesoFin extends javax.swing.JFrame {
     /**
      * @param args the command line arguments
      */
-//    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        /* Create and display the form */
-//        java.awt.EventQueue.invokeLater(new Runnable() {
-//            public void run() {
-//                new VenProcesoFin().setVisible(true);
-//            }
-//        });
-//    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel JReservaNo;
