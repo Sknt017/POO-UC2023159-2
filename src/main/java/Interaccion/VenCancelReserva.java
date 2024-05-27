ef59629dd86e933cb33ae3ab790d3c81e2ed569a
sknt017 <dsanti1882@gmail.com>
2024-05-27 10:22:34 -0500

    perf upt

--- src/main/java/Interaccion/VenCancelReserva.java
+++ src/main/java/Interaccion/VenCancelReserva.java
@@ -115,10 +115,6 @@ public class VenCancelReserva extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_jTextField1ActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
-
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton JBoAtras;
     private javax.swing.JButton jButton1;
